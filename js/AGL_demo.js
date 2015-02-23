f3494c8552326132e76446d2fa754756e284fa43
Marco Morales <mmorale1@jaguarlandrover.com>
2015-02-23 15:44:18 -0800

    Adds app icon loader for top bar and app grid
    Change-Id: I6771b8c6b06e99e3e45e78d52543d0e613786021

--- js/AGL_demo.js
+++ js/AGL_demo.js
@@ -1,41 +1,64 @@
 var apps = {
-	fingerprint: { name: "Fingerprint", image: "fingerprint_inactive.png" },
-	news: { name: "News", image: "news_inactive.png" },
-	browser: { name: "Browser", image: "browser_inactive.png" },
-	voiceprint: { name: "Voiceprint", image: "voiceprint_inactive.png"},
-	phone: { name: "Phone", image: "phone_inactive.png"},
-	navigation: { name: "Navigation", image: "navigation_inactive.png"},
-	email: { name: "Email", image: "email_inactive.png"},
-	dashboard: { name: "Dashboard", image: "dashboard_inactive.png"},
-	musicPlayer: { name: "Music Player", image: "music_player_inactive.png"},
-	nfc: { name: "NFC", image: "nfc_inactive.png"},
-	demo1: { name: "NoSpacesOverflow", image: "tizen_inactive.png"},
-	demo2: { name: "With Spaces Overflow", image: "tizen_inactive.png"}
+	fingerprint: { name: "Fingerprint", 					image: "fingerprint_inactive.png" },
+	news: 			 { name: "News", 									image: "news_inactive.png" },
+	browser: 		 { name: "Browser", 							image: "browser_inactive.png" },
+	voiceprint:  { name: "Voiceprint", 						image: "voiceprint_inactive.png" },
+	phone: 			 { name: "Phone", 								image: "phone_inactive.png" },
+	navigation:  { name: "Navigation", 						image: "navigation_inactive.png" },
+	email: 			 { name: "Email", 								image: "email_inactive.png" },
+	dashboard:   { name: "Dashboard", 						image: "dashboard_inactive.png" },
+	musicPlayer: { name: "Music Player", 					image: "music_player_inactive.png" },
+	nfc: 				 { name: "NFC", 									image: "nfc_inactive.png" },
+	demo1: 			 { name: "NoSpacesOverflow", 			image: "tizen_inactive.png" },
+	demo2: 			 { name: "With Spaces Overflow", 	image: "tizen_inactive.png" }
 };
 
+var homescreenTemplates = document.getElementById('homescreen-templates').import;
 
 function Template(templateImport, id) {
   this.template = templateImport.getElementById(id);
   this.templateContent = document.importNode(this.template.content, true);
 };
 
-function loadAppGrid() {
-	var appGridTemplates = document.getElementById('app-grid-templates').import;
-
+function loadTopBarApps() {
 	for(app in apps) {
-		var template = new Template(appGridTemplates, "app-grid-icon-template");
+		var template = new Template(homescreenTemplates, "topbar-icon-template");
+		var content = template.templateContent;
+		var appSpace = $(".app-space:empty").first();
+		var imagePath = "./images/application_icons/" + apps[app].image;
+		var lastSpaceNum = appSpace.prev().data("app-space") || 0;
+		var appIcon = content.querySelector("img");
+		appIcon.setAttribute("src", imagePath);
+		var appData = content.querySelector(".app-space").dataset;
+		appSpace.attr("data-app-id", apps[app].name);
+		appSpace.attr("data-app-space", lastSpaceNum + 1);
+		appSpace.html(appIcon);
+	};
+};
+
+function loadAppGrid() {
+	for(app in apps) {
+		var template = new Template(homescreenTemplates, "app-grid-icon-template");
 		var content = template.templateContent;
 		var appInfo = apps[app];
 		var imagePath = "./images/application_icons/" + appInfo.image;
 		content.querySelector("img").setAttribute("src", imagePath);
 		content.querySelector(".app-grid-icon-label").textContent = appInfo.name;
 		
-		$(content).insertBefore("#app-grid-apps #add-another-icon");
+		$("#app-grid-apps").append(content);
 	};
 };
 
+// This function is for demo purposes ONLY
+function loadAddIconToAppGrid() {
+	var template = new Template(homescreenTemplates, "add-icon-template");
+	$("#app-grid-apps").append(template.templateContent);
+};
+
 var init = function() {
+	loadTopBarApps();
 	loadAppGrid();
+	loadAddIconToAppGrid();
 };
 
 $(document).ready(function() {
